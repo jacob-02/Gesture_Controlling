25ac3f5f62cee9cbaae2546052ff44ee0bf9d8ac
jackjack3006 <jvsanoj23@gmail.com>
2021-05-06 16:27:27 +0530

    Creating a real Hand Module

--- landmark/HandModule.py
+++ landmark/HandModule.py
@@ -2,24 +2,29 @@ import time
 import mediapipe as mp
 import cv2
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
 
-image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-results = hands.process(image)
+class HandDetector:
+    def __init__(self, mode=False, max_hands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.max_hands = max_hands
+        self.trackCon = trackCon
+        self.detectionCon = detectionCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.max_hands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-if results.multi_hand_landmarks:
-    for handLms in results.multi_hand_landmarks:
-        for id, lm in enumerate(handLms.landmark):
-            h, w, c = frame.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            print(id, cx, cy)
+    def findHands(self, frame, draw=True):
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(image)
 
-        mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS,
-                              mpDraw.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=2),
-                              mpDraw.DrawingSpec(color=(155, 155, 155), thickness=2,
-                                                 circle_radius=2))
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS,
+                                               self.mpDraw.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=2),
+                                               self.mpDraw.DrawingSpec(color=(155, 155, 155), thickness=2,
+                                                                       circle_radius=2))
+        return frame
 
 
 def main():
@@ -27,9 +32,13 @@ def main():
 
     capture = cv2.VideoCapture(0)
 
+    detector = HandDetector()
+
     while True:
         ret, frame = capture.read()
         frame = cv2.flip(frame, 1)
+
+        frame = detector.findHands(frame)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)

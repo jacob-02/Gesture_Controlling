529da9597f7581d70d4c7c52f156d27eb575e454
jackjack3006 <jvsanoj23@gmail.com>
2021-05-07 13:11:48 +0530

    Bugs

--- run.py
+++ run.py
@@ -19,7 +19,9 @@ while True:
     frame = detector.findHands(frame)
 
     lm = detector.findPosition(frame, draw=False)
-    distance=0
+
+    distance = 0
+
     if len(lm) != 0:
         x1, y1 = lm[4][1], lm[4][2]
         x2, y2 = lm[8][1], lm[8][2]
@@ -39,7 +41,7 @@ while True:
         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), thickness=2)
 
         if volume >= 100.0:
-            volume = 100
+            volume = 100.0
 
         cv2.putText(frame, str(int(volume)) + "%", (10, 450), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
@@ -51,7 +53,8 @@ while True:
 
     cv2.imshow('Webcam', frame)
 
-    volume = distance/2.2
+    volume = distance / 2.2
+
     call(["amixer", "-D", "pulse", "sset", "Master", str(volume) + "%"])
 
     if cv2.waitKey(20) & 0xFF == ord('d'):

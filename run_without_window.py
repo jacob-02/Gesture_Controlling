abf434c51cd5854b4c44dda8c1f99356e0ce268d
jackjack3006 <jvsanoj23@gmail.com>
2021-05-07 08:57:53 +0530

    Volume Control without a window popping up

---
+++ run_without_window.py
@@ -0,0 +1,41 @@
+import cv2
+import time
+import math
+from subprocess import call
+from landmark import HandModule
+
+wCam, hCam = 700, 500
+
+capture = cv2.VideoCapture(0)
+capture.set(3, wCam)
+capture.set(4, hCam)
+pTime = 0
+
+detector = HandModule.HandDetector(detectionCon=0.8)
+
+while True:
+    ret, frame = capture.read()
+    frame = detector.findHands(frame)
+
+    lm = detector.findPosition(frame, draw=False)
+    distance=0
+    if len(lm) != 0:
+        x1, y1 = lm[4][1], lm[4][2]
+        x2, y2 = lm[8][1], lm[8][2]
+
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        distance = math.hypot(x2 - x1, y2 - y1)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    volume = 100//400 * distance
+    call(["amixer", "-D", "pulse", "sset", "Master", str(distance) + "%"])
+
+    if cv2.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
